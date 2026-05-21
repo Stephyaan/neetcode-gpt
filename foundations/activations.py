8ce79547f40226523fa9e39726fec90dae6919dd
Stephy Ann Biju <stephyannbiju@gmail.com>
2026-05-21 18:24:19 +0530

    Update activations.py

--- foundations/activations.py
+++ foundations/activations.py
@@ -1,9 +1,8 @@
+#Qn: https://neetcode.io/problems/sigmoid-and-relu/question
+#my answer(simple logic code)
 import numpy as np
 from numpy.typing import NDArray
-
-
-class Solution:
-    
+class Solution:   
     def sigmoid(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
         # z is a 1D NumPy array
         # Formula: 1 / (1 + e^(-z))
@@ -19,3 +18,12 @@ class Solution:
         for x in z:
             result.append(float(max(0,x)))
         return result
+#original code:using numpy library completely
+import numpy as np
+from numpy.typing import NDArray
+class Solution:
+    def sigmoid(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        return np.round(1 / (1 + np.exp(-z)), 5)
+
+    def relu(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        return np.maximum(0, z)
